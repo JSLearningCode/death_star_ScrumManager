41beb6a3e6f4d0a6e3cb434ecd0224d515057a5b
williamguilhermesouza <williamguilhermesouza@gmail.com>
2019-11-26 07:17:18 -0300

    finished crud operations of projects

--- backend/src/controllers/ProjectsController.js
+++ backend/src/controllers/ProjectsController.js
@@ -16,5 +16,35 @@ module.exports = {
         const { proj } = req.headers;
         const projects = await Projects.find({ proj });
         return res.json(projects);
+    },
+
+    async update(req, res) {
+        try {
+            const id = req.body.id;
+            const update = {
+                name: req.body.name,
+                productOwner: req.body.productOwner,
+                scrumMaster: req.body.scrumMaster,
+                team: req.body.team,
+            };
+            await Projects.updateOne(id, update);
+                
+            return res.send('UPDATED');
+        }
+        catch(err) {
+            return res.status(400).send({ error: 'operation failed' });
+        }
+   
+    },
+
+    async delete(req, res) {
+        try {
+            const id = req.body.id;
+            await Projects.deleteOne(id);
+            return res.status(200).send('DELETED');
+        }
+        catch(err) {
+            return res.status(400).send({ error: 'failed to match project'});
+            }
     }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,6 +9,8 @@ routes = express.Router();
 
 //get do dashboard
 
+//READ
+
 routes.get('/dashboard', DashboardController.show);
 
 routes.get('/projectshow', ProjectsController.show);
@@ -19,19 +21,23 @@ routes.get('/usershow', UsersController.show);
 
 // CRUD de usuarios, post de sprints, post de projetos
 
+// CREATE
+
 routes.post('/userscreate', UsersController.store);
 
 routes.post('/sprintscreate', SprintsController.store);
 
 routes.post('/projectscreate', ProjectsController.store);
 
-// routes.post('/projectupdate', ProjectsController.update);
+// UPDATES
+
+routes.post('/projectupdate', ProjectsController.update);
 
 // routes.post('/sprintupdate', SprintsController.update);
 
 // routes.post('/userupdate', UsersController.update);
 
-// routes.post('/projectdelete', ProjectsController.delete);
+routes.post('/projectdelete', ProjectsController.delete);
 
 // routes.post('/sprintdelete', SprintsController.delete);
 
